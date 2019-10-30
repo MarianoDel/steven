78e3526bc158d4d807cc214281185d353410bd64
med <marianodeleu@yahoo.com.ar>
2019-10-30 17:49:22 -0300

    armo bases en mongo

--- mongo_conn.py
+++ mongo_conn.py
@@ -9,32 +9,58 @@ import time
 def ConnMongo ():
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
     mydb = myclient["mydatabase"]
-    mycol = mydb["customers"]
+    mycol_prod = mydb["productos"]
+    mycol_des = mydb["desechos"]
 
-    return mycol
+    return mycol_prod, mycol_des
 
 
-
-def InsertOne (collection, index):
+def InsertMultiple (collection, index):
     mydict = { "name": f"John{index}", "address": "Highway 37" }
     x = collection.insert_one(mydict)
 
 
+def InsertOne (collection, data_dict):
+    x = collection.insert_one(data_dict)
+
+
 def ShowCol (collection):
-    mydoc = collection.find().sort("name")
+    # mydoc = collection.find().sort("name")
+    mydoc = collection.find({},{ "_id": 0, "producto": 1, "descripcion": 1 })
 
     for x in mydoc:
         print(x)
 
+
+def DeleteOne (collection):
+    myquery = { "address": "Mountain 21" }
+    collection.delete_one(myquery)
+
+    
 if __name__ == "__main__":
     print ("Intento conectar con Mongo")
-    mycol = ConnMongo()
+    myprod, mygarb = ConnMongo()
 
-    print ("Agrego datos a collection")
-    for i in range(10):
-        InsertOne(mycol, i)
+    # print ("Agrego datos a collection")
+    # for i in range(10):
+    #     InsertOne(mycol, i)
 
-    print ("Collection data")
-    ShowCol(mycol)
+    # print ("Agrego datos a collection Productos")
+    # mydict = { "producto": "agua", "descripcion": "eco de los andes con gas", "barcode": "77927799000035" }
+    # InsertOne(myprod, mydict)
+
+    # mydict = { "producto": "sopa", "descripcion": "quick de pollo", "barcode": "7794000598584" }
+    # InsertOne(myprod, mydict)
+
+    # mydict = { "producto": "arvejas", "descripcion": "marolio tetra 340g", "barcode": "7797470007426" }
+    # InsertOne(myprod, mydict)
+    
+    print ("Collection data for Productos")
+    ShowCol(myprod)
+
+    print ("Collection data for Desechos")
+    ShowCol(mygarb)
+    
+    
 
     
